65c82bc42646f568b72bd678a3fc90e35d2cae65
Primology <istevkovski@outlook.com>
2018-10-21 22:21:54 +0200

    Refactoring the files

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.27660467bacc16ac2d6a2b207cba7fb2.js"
+  "/Seeksly/precache-manifest.d03d5d5f98610f7b2966c00321d06c01.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/build/index.html", {
+workbox.routing.registerNavigationRoute("/Seeksly/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
