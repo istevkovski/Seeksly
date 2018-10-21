aaf4d54886a9fe2d9261542bbd4f248d4a5ab138
Primology <istevkovski@outlook.com>
2018-10-22 01:09:37 +0200

    Build update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08ebcf57100009f930fbf2c7e7e3f283.js"
+  "/precache-manifest.494174d79162fa1caa6cbb1bdb86f09c.js"
 );
 
 workbox.clientsClaim();
