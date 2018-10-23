a590d829006fc4e736b2862056480a6053a5fd96
Primology <istevkovski@outlook.com>
2018-10-23 06:29:52 +0200

    Updates

---
+++ precache-manifest.2ca223f463139ebda85ef3abd959d7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f79daaebae132d7f6f4",
+    "url": "/static/js/main.0f79daae.chunk.js"
+  },
+  {
+    "revision": "46e55a3ea61ecac02625",
+    "url": "/static/js/1.46e55a3e.chunk.js"
+  },
+  {
+    "revision": "0f79daaebae132d7f6f4",
+    "url": "/static/css/main.f6d146a9.chunk.css"
+  },
+  {
+    "revision": "c4854b3ebf16ae3ea5bc6914ffa96279",
+    "url": "/index.html"
+  }
+];
